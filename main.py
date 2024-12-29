7e0a72e53fcbd44dbf25cbd1dc68de9472313c93
ChrisKim <1515748548@qq.com>
2024-12-29 16:28:38 +0800

    [feat] 支持对数曲线

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ import psutil
 import requests
 
 
-def percent_to_azimuth(percent: float, half: bool = False) -> float:
-    """ convert usage percentage to azimuth angle
-    azimuth: 0 -> up, 90 -> right, 180 -> down, 270 -> left
-    half: 0 -> left, 50 -> up, 100 -> right
-    full: 0 -> left, 25 -> up, 50 -> right, 75 -> down, 100 -> left
+def percent_to_azimuth(percent: float, half: bool = False, log: bool = False) -> float:
+    """convert usage percentage to azimuth angle
+    azimuth : 0 -> up, 90 -> right, 180 -> down, 270 -> left
+    ------
+    half: 1.8deg/pct, 0 -> left, 50 -> up, 100 -> right
+    full: 3.6deg/pct, 0 -> left, 25 -> up, 50  -> right, 75 -> down, 100 -> left
+    ------
+    half log: 0~10 ->   9deg/pct, 10~30 ->  4.5deg/pct, 30~60 ->   3deg/pct, 60~100 ->  2.25deg/pct
+    full log: 0~10 -> 4.5deg/pct, 10~30 -> 2.25deg/pct, 30~60 -> 1.5deg/pct, 60~100 -> 1.125deg/pct
     """
-    if half:
-        return (percent * 1.8 - 90 + 360) % 360
-    else:
-        return (percent * 3.6 - 90 + 360) % 360
+    pct_list = [10, 20, 30, 40]
+    if half == True and log == False:
+        deg_list = [1.8, 1.8, 1.8, 1.8]
+    elif half == False and log == False:
+        deg_list = [3.6, 3.6, 3.6, 3.6]
+    elif half == True and log == True:
+        deg_list = [4.5, 2.25, 1.5, 1.125]
+    else:  # half == False and log == True
+        deg_list = [9, 4.5, 3, 2.25]
+    azimuth = 0.0
+    for i in range(4):
+        azimuth += deg_list[i] * min(percent, pct_list[i])
+        percent -= pct_list[i]
+        if percent <= 0.0:
+            break
+    azimuth = (azimuth - 90 + 360) % 360
+    return azimuth
 
 
 def get_cpu_usage():
@@ -29,13 +46,12 @@ def get_memory_usage():
 def main():
     while True:
         usage = get_cpu_usage() if args.monitor_type == "cpu" else get_memory_usage()
-        azimuth = percent_to_azimuth(usage, half=args.half)
-        loguru.logger.info(f"Usage: {usage}%, Azimuth: {azimuth}deg")
+        azimuth = percent_to_azimuth(usage, half=args.half, log=args.logarithm)
         try:
-            resp = requests.post(f"http://{args.compass_ip}/setAzimuth?azimuth={azimuth}")
-            loguru.logger.debug(f"Status: {resp.status_code}")
+            resp = requests.post(f"http://{args.compass_ip}/setAzimuth?azimuth={azimuth}", timeout=0.5)
+            loguru.logger.info(f"Usage: {usage}%, Azimuth: {azimuth}°, Status: {resp.status_code}")
         except Exception as e:
-            loguru.logger.error(f"Error: {e}")
+            loguru.logger.error(f"Usage: {usage}%, Azimuth: {azimuth}°, Error: {e}")
         time.sleep(args.interval)
 
 
@@ -44,8 +60,9 @@ if __name__ == "__main__":
     parser.add_argument("--compass-ip", type=str, required=True, help="IP address of the MCompass")
     parser.add_argument("--monitor-type", type=str, default="cpu", help="Monitor type: cpu/mem")
     parser.add_argument("--interval", type=float, default=1.0, help="Interval between each update")
-    parser.add_argument("--half", type=bool, default=True, help="Half circle mode")
-    parser.add_argument("--silent", type=bool, default=False, help="Silent mode")
+    parser.add_argument("--silent", action='store_true', help="Silent mode")
+    parser.add_argument("--half", action='store_true', help="Half circle mode")
+    parser.add_argument("--logarithm", action='store_true', help="Logarithmic curve mode")
     args = parser.parse_args()
     if args.silent:
         loguru.logger.remove()
